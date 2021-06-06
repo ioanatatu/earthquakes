be30df3c9a0165cae5550a82767d8da464edad63
ioanatatu <tatu.ioana@gmail.com>
2021-06-06 10:53:10 +0200

    tweak tests for helperFunctions

--- src/helpers/helperFunctions.test.js
+++ src/helpers/helperFunctions.test.js
@@ -1,31 +1,30 @@
 import {
-   findMinMaxByProperty,
-   mapWordMetadata,
-   assignSize,
+   findMaxMag,
+   mapDataOnGlobeMarkers,
    assignColor,
 } from '../helpers/helperFunctions';
-import { wordColors } from './config';
+import { magnitudeColors } from './config';
 import data from '../object.json';
 
-const { green, red, grey } = wordColors;
+const { blue, green, yellowGreen, orange, red } = magnitudeColors;
 
 /**
- * test findMinMaxByProperty
+ * test findMaxMag
  */
 describe('return value if a string', () => {
    it('should return the id as a string', () => {
-      const id = findMinMaxByProperty(data.topics, 'sentimentScore', 'max');
+      const id = findMaxMag(data);
       expect(id).toMatch('');
    });
 });
 /**
- * test mapWordMetadata
+ * test mapDataOnGlobeMarkers
  */
 test('invalid input for word object', () => {
-   const nullOrUndefinedOrEmpty = () => mapWordMetadata();
-   const testString = () => mapWordMetadata('asdasd');
-   const testNumber = () => mapWordMetadata(10);
-   const testArray = () => mapWordMetadata([]);
+   const nullOrUndefinedOrEmpty = () => mapDataOnGlobeMarkers();
+   const testString = () => mapDataOnGlobeMarkers('asdasd');
+   const testNumber = () => mapDataOnGlobeMarkers(10);
+   const testArray = () => mapDataOnGlobeMarkers([]);
 
    expect(nullOrUndefinedOrEmpty).toThrowError(
       new Error('null, undefined or empty object')
@@ -36,107 +35,46 @@ test('invalid input for word object', () => {
 });
 
 test('mapped word object structure', () => {
-   const word = {
-      id: '1751295897__Berlin',
-      label: 'Berlin',
-      volume: 165,
-      type: 'topic',
-      sentiment: {
-         negative: 3,
-         neutral: 133,
-         positive: 29,
+   const quake = {
+      type: 'Feature',
+      properties: {
+         mag: 2.00999999,
+         place: '8 km E of Pāhala, Hawaii',
+         time: 1622900393410,
+         updated: 1622900588030,
+         tz: null,
+         url: 'https://earthquake.usgs.gov/earthquakes/eventpage/hv72506707',
+         detail:
+            'https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/hv72506707.geojson',
+         felt: null,
+         cdi: null,
+         mmi: null,
+         alert: null,
+         status: 'automatic',
+         tsunami: 0,
+         sig: 62,
+         net: 'hv',
+         code: '72506707',
+         ids: ',hv72506707,',
+         sources: ',hv,',
+         types: ',origin,phase-data,',
+         nst: 49,
+         dmin: null,
+         rms: 0.119999997,
+         gap: 151,
+         magType: 'md',
+         type: 'earthquake',
+         title: 'M 2.0 - 8 km E of Pāhala, Hawaii',
       },
-      sentimentScore: 65,
-      burst: 13,
-      days: [
-         {
-            date: '2014-06-06T00:00:00.000+0000',
-            volume: 22,
-         },
-         {
-            date: '2014-06-04T00:00:00.000+0000',
-            volume: 43,
-         },
-         {
-            date: '2014-06-09T00:00:00.000+0000',
-            volume: 0,
-         },
-         {
-            date: '2014-06-07T00:00:00.000+0000',
-            volume: 12,
-         },
-         {
-            date: '2014-06-08T00:00:00.000+0000',
-            volume: 11,
-         },
-         {
-            date: '2014-06-03T00:00:00.000+0000',
-            volume: 39,
-         },
-         {
-            date: '2014-06-05T00:00:00.000+0000',
-            volume: 38,
-         },
-      ],
-      pageType: {
-         blog: 17,
-         facebook: 56,
-         forum: 22,
-         general: 5,
-         image: 0,
-         news: 26,
-         review: 1,
-         twitter: 35,
-         video: 3,
+      geometry: {
+         type: 'Point',
+         coordinates: [-155.399505615234, 19.2158336639404, 32.439998626709],
       },
-      queries: [
-         {
-            id: 1751295897,
-            name: 'Berghain',
-            volume: 165,
-         },
-      ],
+      id: 'hv72506707',
    };
-   expect(mapWordMetadata(word)).toHaveProperty('label');
-   expect(mapWordMetadata(word)).toHaveProperty('volume');
-   expect(mapWordMetadata(word)).toHaveProperty('sentiment');
-});
-
-/**
- * test assignSize
- */
-test('invalid input for size', () => {
-   // refactor these into one function that tests invalid input for positive numbers
-   const nullOrUndefined = () => assignSize();
-   const notNumber = () => assignSize('asdasd');
-   const notPositive = () => assignSize(-1);
-
-   expect(nullOrUndefined).toThrowError(new Error('null or undefined'));
-   expect(notNumber).toThrowError(new Error('not a number'));
-   expect(notPositive).toThrowError(new Error('not a positive number'));
-});
-
-test('correct size attribution', () => {
-   // val > 10 && val <= 30;
-   expect(assignSize(0)).toBe(12);
-   expect(assignSize(10)).toBe(12);
-   expect(assignSize(11)).toBe(16);
-   expect(assignSize(29)).toBe(16);
-   expect(assignSize(30)).toBe(16);
-   // val > 30 && val <= 60;
-   expect(assignSize(31)).toBe(20);
-   expect(assignSize(59)).toBe(20);
-   expect(assignSize(60)).toBe(20);
-   // val > 60 && val <= 90;
-   expect(assignSize(61)).toBe(24);
-   expect(assignSize(89)).toBe(24);
-   expect(assignSize(90)).toBe(24);
-   // val > 90 && val <= 120;
-   expect(assignSize(91)).toBe(28);
-   expect(assignSize(119)).toBe(28);
-   expect(assignSize(120)).toBe(28);
-   // val > 120;
-   expect(assignSize(121)).toBe(30);
+   expect(mapDataOnGlobeMarkers(quake)).toHaveProperty('label');
+   expect(mapDataOnGlobeMarkers(quake)).toHaveProperty('volume');
+   expect(mapDataOnGlobeMarkers(quake)).toHaveProperty('sentiment');
 });
 
 /**
